662bc26fe48538d920e59ac4170850c23afdbde5
Akbar <tillayevakbarshoh@gmail.com>
2026-08-22 18:13:50 +0500

    Revert "Proxy backend API requests"
    This reverts commit 036a6f051540452b8de4a19c5012463d6a41f91c.

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: '/backend-api/:path*',
+        destination: 'https://api.cconnect.uz/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
