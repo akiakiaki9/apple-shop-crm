036a6f051540452b8de4a19c5012463d6a41f91c
Akbar <tillayevakbarshoh@gmail.com>
2026-08-22 16:42:14 +0500

    Proxy backend API requests

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: true,
-  async rewrites() {
-    return [
-      {
-        source: '/backend-api/:path*',
-        destination: 'https://api.cconnect.uz/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
