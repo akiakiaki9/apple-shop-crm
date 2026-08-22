9fb3d2de488e7611ce0503f9e8602c4a5acbb679
Akbar <tillayevakbarshoh@gmail.com>
2026-08-22 18:13:50 +0500

    Revert "Proxy API through Vercel"
    This reverts commit cbec67b0ca065f264a72e02b8d2a4ac1c66f423a.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/backend-api/:path*',
-        destination: 'https://api.cconnect.uz/:path*',
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
